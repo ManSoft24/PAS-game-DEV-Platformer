82f820427bd5c3ee11a45c84efeea63e395b33a5
ManSoft24 <reiyanrifki1@gmail.com>
2025-11-11 10:30:18 +0700

    initial commit + movement + jump

---
+++ gamePASgameDEV/Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    public SpriteRenderer sr; 
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        if (moveInput != 0 && moveInput > 0)
+       {
+           sr.flipX = false;
+       }
+       else if (moveInput != 0 && moveInput < 0)
+       {
+           sr.flipX = true;
+           
+       }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+}
