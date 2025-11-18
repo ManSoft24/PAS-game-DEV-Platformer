1b5c9759f62c5aac105b36a8e1d529d6f7c951d6
ManSoft24 <reiyanrifki1@gmail.com>
2025-11-18 10:47:56 +0700

    Fix Player Movement + Double Jump + Cinemachine
    fix movement, when player walk they stuck.

--- gamePASgameDEV/Assets/Scripts/PlayerMovements.cs
+++ gamePASgameDEV/Assets/Scripts/PlayerMovements.cs
@@ -14,6 +14,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
     private Animator animator;  
+    public int maxJumps = 1;
+    private int currentJumps;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,13 +24,28 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void Update()
     {
+        // basic move code and Jumping
+
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            if (isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+            else if(currentJumps > 0)
+            {
+
+                currentJumps--; // ngurangin token jumping
+                // 
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0); //biar gaya nya gk numpuk
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
         }
+
+        // flip sprite for animation
 
         if (moveInput != 0 && moveInput > 0)
         {
@@ -45,11 +62,25 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // ground checker
+        
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+
+
+        // to reset jumps when grounded
+
+        if(isGrounded)
+        {
+            currentJumps = maxJumps;
+        }
     }
+
+    
 
     private void SetAnimation(float moveInput)
     {
+        // animation
         if (isGrounded)
         {
             if (moveInput != 0)
@@ -72,6 +103,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
                 animator.Play("Falling");
             }
         }
+        
     }
     
 }
